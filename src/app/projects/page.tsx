4576ce99ae8708ae5e40428297e0e6b68be703dd
Vasilis Arampatzis <arabatzis.vasileios@gmail.com>
2026-05-11 02:59:42 +0300

    new file:   src/app/projects/page.tsx

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,13 @@
+import ProjectSection from "@/sections/projects";
+
+export const dynamic = "force-static";
+
+export default function Projects() {
+  return (
+    <main>
+      <ProjectSection />
+    </main>
+  );
+}
+
+

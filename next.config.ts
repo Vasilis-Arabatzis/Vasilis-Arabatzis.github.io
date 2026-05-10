31796ef155920a9fd7c28122ce45605170b98219
Vasilis Arampatzis <arabatzis.vasileios@gmail.com>
2026-05-11 01:22:27 +0300

    Changes to be committed: 	modified:   next.config.ts 	modified:   package.json 	modified:   src/sections/contact.tsx 	modified:   src/sections/projects.tsx  Untracked files: 	src/app/projects/

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,20 @@
 import type { NextConfig } from "next";
 
+const repo = "my-cv";
+
 const nextConfig: NextConfig = {
   /* config options here */
 
   reactCompiler: true,
+
+  output: "export",
   
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 
--- src/sections/contact.tsx
+++ src/sections/contact.tsx
@@ -2,10 +2,6 @@
 
 import Image from "next/image";
 import { motion } from "framer-motion";
-import {Mail} from "lucide-react";
-import githubIcon from "../assets/github.ico";
-import linkedinIcon from "../assets/linkedin.ico";
-import Link from "next/link";
 
 
 export default function Contact() {
